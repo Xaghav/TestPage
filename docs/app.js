824204704a6500ebc3d0d3f17393b3b0cf84feeb
Xaghav <102346864+Xaghav@users.noreply.github.com>
2026-02-01 17:29:33 -0800

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -33,9 +33,9 @@ async function searchGuild() {
 
 async function fetchGuildProfile(guildName) {
     const apiUrl = `https://query.idleclans.com/api/Clan/logs/clan/${guildName}`;
-    const proxied = `https://thingproxy.freeboard.io/fetch/${apiUrl}`; //previous proxy corsproxy.io stoped working
+    // const proxied = `https://thingproxy.freeboard.io/fetch/${apiUrl}`; //previous proxy corsproxy.io stoped working
 
-    const res = await fetch(proxied);
+    const res = await fetch(apiUrl);
     if (!res.ok) throw new Error("Guild not found");
 
     return await res.json();
@@ -43,9 +43,9 @@ async function fetchGuildProfile(guildName) {
 
 async function fetchTop10Fallback() {
     const url = "https://query.idleclans.com/api/ClanCup/leaderboard/Default/Woodcutting";
-    const proxied = `https://thingproxy.freeboard.io/fetch/${apiUrl}`;
+    // const proxied = `https://thingproxy.freeboard.io/fetch/${apiUrl}`;
 
-    const res = await fetch(proxied);
+    const res = await fetch(apiUrl);
     if (!res.ok) throw new Error("Failed to fetch fallback leaderboard");
 
     const data = await res.json();
